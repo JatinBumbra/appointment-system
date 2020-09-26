d81465442a9f37828b0e51d5113028984fa22089
Jatin Bumbra <jatin.bumbra.23@gmail.com>
2020-09-27 04:44:37 +0530

    Deploy attempt 3

---
+++ config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: 'AIzaSyAvz5S0I-C4CfI4gbfIwUe59uoLDbRWQwU',
+  authDomain: 'let-sappoint.firebaseapp.com',
+  databaseURL: 'https://let-sappoint.firebaseio.com',
+  projectId: 'let-sappoint',
+  storageBucket: 'let-sappoint.appspot.com',
+  messagingSenderId: '470745537458',
+  appId: '1:470745537458:web:b4e41e507f483d5d2da2ab',
+};
