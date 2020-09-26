f7b9fb637026f0b7e988a3f6e40525e572a606f8
Jatin Bumbra <jatin.bumbra.23@gmail.com>
2020-09-27 04:26:26 +0530

    Deployment attempt 1

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const serveStatic = require('serve-static');
+const path = require('path');
 // Init app
 const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+// Set static folder and serve index.html
+app.use(express.static('client/dist'));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/client/dist/index.html'))
+);
 // Routes
 app.use(require('./routes/events'));
 
